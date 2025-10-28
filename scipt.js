ec3454eb78f2095f2a707bd3a5707e6282a5e66d
ayouberrak <ayouberrak30@gmail.com>
2025-10-28 15:11:00 +0100

    ajout des variables dans js

---
+++ scipt.js
@@ -0,0 +1,29 @@
+let search = document.getElementById('search-filter');
+
+let prevWeek = document.getElementById('prev-month-btn');
+let thisWeek = document.getElementById('current-month-year');
+let nextWeek = document.getElementById('next-month-btn');
+
+let calendrier = document.getElementById('calendar-grid');
+let reservationModel = document.getElementById('reservation-modal');
+let stopReservation = document.getElementById('close-modal-btn');
+let titreModal = document.getElementById('modal-title');
+
+let formEdit= document.getElementById('form-edit-id');
+let formAjout = document.getElementById('form-jour');
+
+let formReservation =document.getElementById('reservation-form');
+
+let NonClient = document.getElementById('nom-client');
+let heureDebut = document.getElementById('heure-debut');
+let heureFin = document.getElementById('heure-fin');
+let nbrPresonnes = document.getElementById('nombre-personnes');
+let typeReservation = document.getElementById('type-reservation');
+
+
+let cancel =document.getElementById('cancel-btn');
+
+
+document.addEventListener('DOMContentLoaded' , function(){
+
+})
